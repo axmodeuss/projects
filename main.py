c804ce72bd662770c1db7a6fd02fdee618391128
pouyan <pouyan.h1379@gmail.com>
2026-07-12 14:34:09 +0200

    make uv init to let have venv and added a new prooject that change text to spech

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from simple-projects!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ text_to_speach.py
@@ -0,0 +1,7 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+text = input("Enter Text : ")
+
+engine.say(text)
+engine.runAndWait()
